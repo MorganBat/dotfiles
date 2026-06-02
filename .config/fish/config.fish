96e433c941db02431dee3a725b8b8b206ad6d420
Morgan Batterham <morgan.batterham@clio.com>
2026-06-02 11:58:06 +1000

    move aliased_functions to fish device config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,18 +9,10 @@ if status is-interactive
     . (rbenv init -|psub)
 
     nvm use latest > /dev/null
-
-    # Alias functions to use zsh instead
-    set aliased_functions claude
-
-    for function_name in aliased_functions
-        function $function_name --inherit-variable function_name
-            env SHELL=(which zsh) command $function_name $argv
-        end
-    end
 end
 
 set -g PATH $HOME/.rbenv/bin $PATH
 set -g PATH $HOME/.local/bin $PATH
 
 # direnv hook fish | source
+
